5533c9906b687a329e80682944327f33376f57ed
beranju <beranjusihombing1@gmail.com>
2023-03-20 02:03:37 +0700

    create font manager

---
+++ lib/common/font_manager.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+import 'package:google_fonts/google_fonts.dart';
+
+
+class MyFont{
+  static String fontFamily = GoogleFonts.poppins.toString();
+}
+
+class FontWeightManager{
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
