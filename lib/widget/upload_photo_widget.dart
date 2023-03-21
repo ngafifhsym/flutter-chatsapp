1ee1998ad40a0fccd712f2f8949000d990f47841
beranju <beranjusihombing1@gmail.com>
2023-03-21 14:08:45 +0700

    create upload photo widget

---
+++ lib/widget/upload_photo_widget.dart
@@ -0,0 +1,49 @@
+
+import 'dart:io';
+
+import 'package:chatapp/common/color_manager.dart';
+import 'package:flutter/material.dart';
+
+class UploadPhotoWidget extends StatelessWidget {
+  const UploadPhotoWidget({Key? key, required this.image}) : super(key: key);
+
+  final File? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                  color: ColorManager.brown,
+                  width: 8,
+                  strokeAlign: BorderSide.strokeAlignOutside)),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: image != null ? Image(image: FileImage(image!), fit: BoxFit.cover,) : Image.asset('assets/images/empty_image.png', fit: BoxFit.cover,),
+          ),
+        ),
+        Positioned(
+          bottom: -25,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: RawMaterialButton(
+              fillColor: ColorManager.secondaryColor,
+              elevation: 2.0,
+              padding: const EdgeInsets.all(16),
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
