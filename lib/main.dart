430c94c1ad4deafb4f0f3dd99dd466843990d0d8
unknown <ngafifhasyim209@gmail.com>
2023-03-30 09:33:14 +0700

    Add profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:faker/faker.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Myapp());
 }
-class MyApp extends StatelessWidget {
-  var faker = new Faker();
-  List<Tab> myTab = [
-    Tab(
-      text: "Chats",
-    ),
-    Tab(
-      text: "Whats New",
-    )
-  ];
+
+class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: myTab.length,
-        child: Scaffold(
-          backgroundColor: Color(0xFF111E27),
-          appBar: AppBar(
-            title: Text("Walchat"),
-            backgroundColor: Color(0xFF202D36),
-            bottom: TabBar(
-              tabs: myTab,
-              indicatorColor: Color(0xFF6D493A),
-            ),
-          ),
-          body: TabBarView(children: [
-             ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-           return ChatItem(
-              imageUrl: "https://picsum.photos/id/$index/200/300",
-              title: faker.person.name(),
-              subtitle: faker.lorem.sentence(),
-              );
-          },
-        ),
-            Center(
-              child: Text("Text 2"),
-            ),
-        //  
-          ])
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
-class ChatItem extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String subtitle;
-
-  const ChatItem({
-    super.key,
-    required this.imageUrl, required this.title, required this.subtitle,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      textColor: Colors.white,
-      title: Text(title),
-      subtitle: Text(subtitle, maxLines: 1, overflow: TextOverflow.ellipsis),
-      leading: CircleAvatar(backgroundImage: NetworkImage(imageUrl),),
-      trailing: Text("10.00 PM"),
-      
-    );
-    }
-    }
