52e6f02cd42667eeba7bf7662c5206d3b9e0c2be
beranju <beranjusihombing1@gmail.com>
2023-04-04 11:14:19 +0700

    create video picker dan file picker

--- lib/widget/chat_text_field.dart
+++ lib/widget/chat_text_field.dart
@@ -1,5 +1,4 @@
-import 'dart:io';
-
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -21,33 +20,35 @@ class CustomChatTextField extends StatelessWidget {
       child: Row(
         children: [
           IconButton(
-              onPressed: () {
-                selectImage(ImageSource.gallery);
-              },
-              icon: const Icon(
-                Icons.emoji_emotions_rounded,
-                color: Colors.white,
-              )),
+            onPressed: () {
+              _showPopupMenu(context);
+            },
+            icon: const Icon(
+              Icons.attach_file,
+              color: Colors.white,
+            ),
+          ),
           Flexible(
-              child: TextField(
-            maxLines: 5,
-            minLines: 1,
-            style: getWhite14RegularTextStyle(),
-            keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
-                hintText: 'Message',
-                hintStyle: getWhite14RegularTextStyle(),
-                filled: true,
-                border: InputBorder.none),
-          )),
+            child: TextField(
+              maxLines: 5,
+              minLines: 1,
+              style: getWhite14RegularTextStyle(),
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                  hintText: 'Message',
+                  hintStyle: getWhite14RegularTextStyle(),
+                  filled: true,
+                  border: InputBorder.none),
+            ),
+          ),
           IconButton(
-              onPressed: () {
-                _showPopupMenu(context);
-              },
-              icon: const Icon(
-                Icons.attach_file,
-                color: Colors.white,
-              ))
+            onPressed: () {
+            },
+            icon: const Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+          )
         ],
       ),
     );
@@ -56,27 +57,65 @@ class CustomChatTextField extends StatelessWidget {
   void _showPopupMenu(BuildContext context) async {
     final size = MediaQuery.of(context).size;
     await showMenu(
-        context: context,
-        position: RelativeRect.fromLTRB(size.width, size.height, 10, 100),
-        items: [
-          PopupMenuItem(
-              child: GestureDetector(
-                onTap: (){
-                  selectImage(ImageSource.gallery);
-                },
-                  child: const Text('Image')
-              )
+      context: context,
+      color: ColorManager.secondaryColor,
+      position: RelativeRect.fromLTRB(10, size.height, size.width, 100),
+      items: [
+        PopupMenuItem(
+          child: TextButton.icon(
+            onPressed: () {
+              selectImage(ImageSource.gallery);
+            },
+            icon: const Icon(Icons.image),
+            label: Text(
+              'Image',
+              style: getWhite14RegularTextStyle(),
+            ),
           ),
-          PopupMenuItem(child: Text('File')),
-          PopupMenuItem(child: Text('Video')),
-        ],
-        elevation: 1);
+        ),
+        PopupMenuItem(
+          child: TextButton.icon(
+            onPressed: () {
+              selectFile();
+            },
+            icon: const Icon(Icons.file_present_sharp),
+            label: Text(
+              'File',
+              style: getWhite14RegularTextStyle(),
+            ),
+          ),
+        ),
+        PopupMenuItem(
+          child: TextButton.icon(
+            onPressed: () {
+              selectVideo(ImageSource.gallery);
+            },
+            icon: const Icon(Icons.video_collection),
+            label: Text(
+              'Video',
+              style: getWhite14RegularTextStyle(),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   Future<XFile?> selectImage(ImageSource source) async {
-    final image = await ImagePicker().pickImage(source: source, maxHeight: 1800, maxWidth: 1800);
+    final image = await ImagePicker()
+        .pickImage(source: source, maxHeight: 1800, maxWidth: 1800);
     return image;
   }
+
+  Future<XFile?> selectVideo(ImageSource source) async {
+    final video = await ImagePicker().pickVideo(source: source);
+    return video;
+  }
+
+  Future<List<XFile>?> selectFile() async {
+    List<XFile>? file;
+    final result = await FilePicker.platform.pickFiles(allowMultiple: true);
+    file =  result?.paths.map((e) => XFile(e!)).toList();
+    return file;
+  }
 }
-
-
