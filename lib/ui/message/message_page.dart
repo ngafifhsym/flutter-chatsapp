daed5156a84aeb5b8c16e9c48c5592e78d1677e2
beranju <beranjusihombing1@gmail.com>
2023-04-04 11:47:14 +0700

    add pop up menu in app bar of message page

--- lib/ui/message/message_page.dart
+++ lib/ui/message/message_page.dart
@@ -2,7 +2,8 @@ import 'package:chatapp/common/color_manager.dart';
 import 'package:chatapp/common/style_manager.dart';
 import 'package:chatapp/data/model/message.dart';
 import 'package:chatapp/widget/chat_text_field.dart';
-import 'package:chatapp/widget/message_item_widget.dart';
+import 'package:chatapp/widget/current_message_item.dart';
+import 'package:chatapp/widget/guest_message_item.dart';
 import 'package:flutter/material.dart';
 
 class MessagePage extends StatefulWidget {
@@ -45,19 +46,61 @@ class _MessagePageState extends State<MessagePage> {
             )
           ],
         ),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSettingPopupMenu(context);
+            },
+            icon: const Icon(Icons.menu),
+          ),
+        ],
       ),
       body: Column(
         children: [
-          Expanded(child: ListView.builder(
+          Expanded(
+              child: ListView.builder(
             itemCount: dummyMessage.length,
-            itemBuilder: (context, index){
-              return MessageItem(message: dummyMessage[index]);
+            itemBuilder: (context, index) {
+              bool isCurrent = dummyMessage[index].sender == 'joko';
+              if (isCurrent) {
+                return CurrentMessageItem(message: dummyMessage[index]);
+              } else {
+                return GuestMessageItem(message: dummyMessage[index]);
+              }
             },
           )),
           const CustomChatTextField()
         ],
       ),
+    );
+  }
+
+  void showSettingPopupMenu(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    showMenu(
+      context: context,
+      color: ColorManager.secondaryColor,
+      position: RelativeRect.fromLTRB(size.width, 92, 0, size.height),
+      items: [
+        PopupMenuItem(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Setting',
+              style: getWhite14RegularTextStyle(),
+            ),
+          ),
+        ),
+        PopupMenuItem(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Profile',
+              style: getWhite14RegularTextStyle(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
