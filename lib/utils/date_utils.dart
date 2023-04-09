d9b01773ca9a120b550bc7b564741b9dd4cdac92
beranju <beranjusihombing1@gmail.com>
2023-04-09 16:44:51 +0700

    changed the required input to milisecondepoch time

--- lib/utils/date_utils.dart
+++ lib/utils/date_utils.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
-String dateFormatter(Timestamp date){
-  return DateFormat.Hm().format(date.toDate());
+String dateFormatter(int date){
+  DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(date);
+  return DateFormat.Hm().format(dateTime);
 }
