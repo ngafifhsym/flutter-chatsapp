7759103f72ae009226899cd82abc9501b9cf9698
beranju <beranjusihombing1@gmail.com>
2023-04-12 23:19:03 +0700

    changed user collection reference

--- lib/ui/auth/register/update_photo_page.dart
+++ lib/ui/auth/register/update_photo_page.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:chatapp/common/style_manager.dart';
+import 'package:chatapp/data/cubit/auth_cubit.dart';
 import 'package:chatapp/ui/auth/login/login_screen.dart';
 import 'package:chatapp/ui/home/home_page.dart';
 import 'package:chatapp/widget/custom_button.dart';
 import 'package:chatapp/widget/upload_photo_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UpdatePhotoPage extends StatelessWidget {
   static const String routeName = '/update-photo';
@@ -12,6 +16,13 @@ class UpdatePhotoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final dataUser = ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+
+    void signUpUser(String name, String email, String password, File? imageUrl, String? about) {
+      context.read<AuthCubit>().signUpWithEmail(name, email, password, imageUrl, about);
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -29,7 +40,12 @@ class UpdatePhotoPage extends StatelessWidget {
                   }),
               TextButton(
                   onPressed: () {
-                    Navigator.pushReplacementNamed(context, LoginPage.routeName);
+                    final name = dataUser['name'];
+                    final email = dataUser['email'];
+                    final password = dataUser['password'];
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(name.toString())));
+                    signUpUser(name!, email!, password!, null, null);
+                    Navigator.pushNamedAndRemoveUntil(context, HomePage.routeName, (routeName) => routeName.isFirst);
                   },
                   child: Text(
                     'Skip',
