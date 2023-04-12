1fe3800d3f3c9ef02b6f4f006577039ffe49768b
beranju <beranjusihombing1@gmail.com>
2023-04-13 00:20:20 +0700

    optimized ui of login page

--- lib/ui/auth/login/login_screen.dart
+++ lib/ui/auth/login/login_screen.dart
@@ -1,115 +1,90 @@
+import 'package:chatapp/common/color_manager.dart';
+import 'package:chatapp/common/style_manager.dart';
 import 'package:chatapp/ui/home/home_page.dart';
+import 'package:chatapp/widget/custom_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   static const String routeName = '/login-page';
+
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _emailController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF111E27),
-        body:
-        SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: [
-                const Image(image: AssetImage("assets/images/waletlogo.png"), width: 200, height: 200,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 41),
-                  child: TextField(
-                    enableInteractiveSelection: true,
-                    autofocus: true,
-                    showCursor: true,
-                    cursorColor: Color(0xFF6D493A),
-                    decoration: new InputDecoration(
-                        border: OutlineInputBorder(),
-                        focusedBorder:OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color(0xFF6D493A)
-                            )
-                        ),
-                        enabledBorder:OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromARGB(255, 253, 253, 253)
-                            )
-                        ),
-                        hintText: "Masukan Email Anda",
-                        hintStyle: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                        labelText: "Email",
-                        labelStyle: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                        prefixIcon: Icon(Icons.person),
-                        prefixIconColor: Color.fromARGB(255, 255, 255, 255)
-                    ),
-                    style: TextStyle(
-                        color: Color(0xFF6D493A)
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding:EdgeInsets.symmetric(vertical: 10, horizontal: 41),
-                  child: TextField(
-                    obscureText: true,
-
-                    decoration: new InputDecoration(
-                        focusedBorder:OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color(0xFF6D493A)
-                            )
-                        ),
-                        enabledBorder:OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromARGB(255, 253, 253, 253)
-                            )
-                        ),
-                        border: OutlineInputBorder(),
-                        hintText: "Masukan Password Anda",
-                        hintStyle: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                        labelText: "Password",
-                        labelStyle: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                        prefixIcon: Icon(Icons.key),
-                        prefixIconColor: Color.fromARGB(255, 255, 255, 255)),
-
-
-                    style: TextStyle(
-                        color: Color(0xFF6D493A)
-                    ),
-
-
-
-                  ),
-                ),
-                Container(
-                  width: 200,
-                  height: 45,
-                  margin: EdgeInsets.symmetric(vertical: 60),
-                  child: TextButton(
-                      style: TextButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)
-                          ),
-                          backgroundColor: Color(0xFF6D493A)
-                      ),
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(context, HomePage.routeName);
-                      },
-                      child: Text("Masuk",
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255)
-                        ),)),
-                ),
-                Text("Belum Punya Akun ?", style: GoogleFonts.poppins(fontSize: 16,color: Colors.white)),
-                TextButton(onPressed: (){
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 20, bottom: 20),
+              child: const Image(
+                image: AssetImage("assets/images/waletlogo.png"),
+                width: 200,
+                height: 200,
+              ),
+            ),
+            CustomTextField(
+                labelName: 'Email',
+                textHint: 'Masukkan email anda',
+                controller: _emailController),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+                labelName: 'Password',
+                textHint: 'Password',
+                controller: _passwordController),
+            Container(
+              width: 200,
+              height: 45,
+              margin: const EdgeInsets.only(top: 40, bottom: 50),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    backgroundColor: ColorManager.brown),
+                onPressed: () {
                   Navigator.pushReplacementNamed(context, HomePage.routeName);
                 },
-                    child: Text("Register",style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),))
-              ],
-
+                child: Text("Masuk", style: getWhite16SemiBoldTextStyle()),
+              ),
             ),
-          ),
-        )
-
-    );
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Belum Punya Akun ?", style: getWhite12RegularTextStyle()),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(context,
+                        HomePage.routeName, (routeName) => routeName.isFirst);
+                  },
+                  child: Text(
+                    "SignUp",
+                    style: getWhite14RegularTextStyle(),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
