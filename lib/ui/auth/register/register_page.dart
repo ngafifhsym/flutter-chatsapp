a4e5efffd8013266c71594e0d48a2a1c021035ad
beranju <beranjusihombing1@gmail.com>
2023-04-13 13:38:46 +0700

    read method signup

--- lib/ui/auth/register/register_page.dart
+++ lib/ui/auth/register/register_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:chatapp/common/color_manager.dart';
 import 'package:chatapp/common/style_manager.dart';
 import 'package:chatapp/ui/auth/login/login_screen.dart';
@@ -5,6 +7,9 @@ import 'package:chatapp/ui/auth/register/update_photo_page.dart';
 import 'package:chatapp/widget/custom_button.dart';
 import 'package:chatapp/widget/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../data/cubit/auth_cubit.dart';
 
 class RegisterPage extends StatefulWidget {
   static const String routeName = '/register-page';
@@ -19,6 +24,7 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController nameController = TextEditingController();
+  bool isLoading = false;
 
   @override
   void dispose() {
@@ -36,92 +42,110 @@ class _RegisterPageState extends State<RegisterPage> {
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/images/waletlogo.png',
-                  width: 160,
-                  height: 160,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomTextField(
-                    labelName: "Nama",
-                    textHint: "Masukkan Nama Kamu",
-                    controller: nameController),
-                const SizedBox(
-                  height: 16,
-                ),
-                CustomTextField(
-                    labelName: "Email",
-                    textHint: "Masukkan Email Kamu",
-                    controller: emailController),
-                const SizedBox(
-                  height: 16,
-                ),
-                CustomTextField(
-                  labelName: "Password",
-                  textHint: "Masukkan Password Kamu",
-                  controller: passwordController,
-                  obSecure: true,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                CustomButton(
-                    textButton: "Daftar",
-                    onTap: () {
-                      final name = nameController.text;
-                      final email = emailController.text;
-                      final password = passwordController.text;
-                      if (name.isNotEmpty &&
-                          email.isNotEmpty &&
-                          password.isNotEmpty) {
-                        final Map<String, String> data = {
-                          'name': name,
-                          'email': email,
-                          'password': password
-                        };
-                        Navigator.pushNamed(context, UpdatePhotoPage.routeName,
-                            arguments: data);
-                      } else {
-                        /** jangan lupa menambahakan validasi otomatis menggunakan...
-                         * ...text editing controller listener
-                         */
+            child: BlocBuilder<AuthCubit, AuthState>(
+              builder: (context, state) {
+                return Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/waletlogo.png',
+                      width: 160,
+                      height: 160,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    CustomTextField(
+                        labelName: "Nama",
+                        textHint: "Masukkan Nama Kamu",
+                        controller: nameController),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomTextField(
+                        labelName: "Email",
+                        textHint: "Masukkan Email Kamu",
+                        controller: emailController),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomTextField(
+                      labelName: "Password",
+                      textHint: "Masukkan Password Kamu",
+                      controller: passwordController,
+                      obSecure: true,
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Visibility(visible: isLoading, child: CircularProgressIndicator()),
+                    CustomButton(
+                        textButton: "Daftar",
+                        onTap: () {
+                          final name = nameController.text;
+                          final email = emailController.text;
+                          final password = passwordController.text;
+                          if (name.isNotEmpty &&
+                              email.isNotEmpty &&
+                              password.isNotEmpty) {
+                            context.read<AuthCubit>().signUpWithEmail(name, email, password, null);
+                            if (state is AuthSuccess){
+                              Navigator.pushNamed(
+                                  context, UpdatePhotoPage.routeName,);
+                            }
+                            if (state is AuthLoading){
+                              setState(() {
+                                isLoading != isLoading;
+                              });
+                            }
+                            if (state is AuthFailed){
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text(
+                                  state.error,
+                                  style: getWhite14RegularTextStyle(),
+                                ),
+                                backgroundColor: ColorManager.secondaryColor,
+                              ));
+                            }
 
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(
-                            'Field tidak boleh kosong',
+                          } else {
+                            /** jangan lupa menambahakan validasi otomatis menggunakan...
+                             * ...text editing controller listener
+                             */
+
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(
+                                'Field tidak boleh kosong',
+                                style: getWhite14RegularTextStyle(),
+                              ),
+                              backgroundColor: ColorManager.secondaryColor,
+                            ));
+                          }
+                        }),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Sudah punya akun?',
+                          style: getWhite12RegularTextStyle(),
+                        ),
+                        const SizedBox(width: 5,),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, LoginPage.routeName);
+                          },
+                          child: Text(
+                            'Login',
                             style: getWhite14RegularTextStyle(),
                           ),
-                          backgroundColor: ColorManager.secondaryColor,
-                        ));
-                      }
-                    }),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Sudah punya akun?',
-                      style: getWhite12RegularTextStyle(),
+                        )
+                      ],
                     ),
-                    const SizedBox(width: 5,),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, LoginPage.routeName);
-                      },
-                      child: Text(
-                        'Login',
-                        style: getWhite14RegularTextStyle(),
-                      ),
-                    )
                   ],
-                ),
-              ],
+                );
+              },
             ),
           ),
         ),
